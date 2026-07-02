fc82a59d937112c947fa8888aaf229ff78a4bd3c
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-02 16:08:35 +0200

    Added navbar folder and tsx style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="">
         <h1>Hello my name is sam</h1>
-        <h2 className="">I am a humble guy bakithi</h2>
+        <h2 className="">Testing deployment</h2>
       </div>
     </>
   );
