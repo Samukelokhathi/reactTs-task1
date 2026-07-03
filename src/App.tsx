b641102929f80d1811c1f14ed12b781df936b089
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-03 16:03:48 +0200

    Added NavBar folder and nav.tsc file also nav css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Text } from "./components/Text/Text.tsx";
+// import { Text } from "./components/Text/Text.tsx";
+import Navbar from "./components/Navbar/Navbar.tsx";
 
 export default function App() {
   return (
     <div>
-      <Text variant="h1">Hello</Text>
-      <Text variant="h2">Hello</Text>
-      <Text variant="p">Hello</Text>
-      <Text variant="span">Hello</Text>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,26 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+import { Text } from "../Text/Text";
+import styles from "./Navbar.module.css";
 
-export default Navbar;
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="{style.content}">
+        <Text variant={"h2"}>Sam</Text>
+        <div className={styles.links}>
+          <a href="/#" className={styles.link}>
+            Shop Online
+          </a>
+          <a href="/#" className={styles.link}>
+            Find in Store
+          </a>
+          <a href="/#" className={styles.link}>
+            Learn More
+          </a>
+        </div>
+        <div>
+          <button>Subtribe</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
