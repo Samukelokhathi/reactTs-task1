580a6f355cf803e30b9f726aabd3c0d1276c8881
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-06 11:43:21 +0200

    deleted html style and update to use html elmentns not div to separate the van

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,27 +4,22 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <nav>
-      <div className={styles.content}>
-        <img
-          src="src/assets/Logo Image.jpg"
-          alt="logoImage"
-          height={40}
-          width={40}
-        />
-        <div className={styles.links}>
-          <a href="/#" className={styles.link}>
-            Shop Online
-          </a>
-          <a href="/#" className={styles.link}>
-            Find in Store
-          </a>
-          <a href="/#" className={styles.link}>
-            Learn More
-          </a>
-        </div>
-        <div className={styles.subContainer}>
-          <button>Subscribe</button>
-        </div>
+      <div className={styles.logo}>
+        <img src="src/assets/Logo Image.jpg" alt="logo" />
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <a href="#">Shop Online</a>
+        </li>
+        <li>
+          <a href="#"></a>Find in Store
+        </li>
+        <li>
+          <a href="#"></a>Learn More
+        </li>
+      </ul>
+      <div className={styles.btns}>
+        <button className={styles.btn}>Subscribe</button>
       </div>
     </nav>
   );
