b76eb2d0310b2c81059f5033d0f511db90d429ec
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-06 15:29:06 +0200

    Created Btn and Hero components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import { Text } from "./components/Text/Text.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
+import Hero from "./components/Hero/Hero.tsx";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+// // import React from "react";
+// import btnCss from "./Button.css";
+
+// export default function Button() {
+//   return (
+//     <div>
+//       {" "}
+//       <button></button>
+//     </div>
+//   );
+// }
