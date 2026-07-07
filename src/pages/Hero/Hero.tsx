58a10cb1f35e46964bf2a5c3058df5b674ec2e4f
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-07 10:38:12 +0200

    Updating css, just targeted section elements

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
         <div className={heroStyle.heroContent}>
           <h1>A new kind \n of Soda</h1>
         </div>
-        <div className="heroStyle.image-container">
+        <div className={heroStyle.imageContainer}>
           <img src="src/assets/heroImage.jpg" alt="" width={400} height={400} />
         </div>
       </section>
