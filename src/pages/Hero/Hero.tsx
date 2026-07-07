6f0e9e198e898d881c897f51a2d8918963c94e69
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-07 12:03:32 +0200

    updated hero section css

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -5,7 +5,9 @@ export default function Hero() {
     <>
       <section>
         <div className={heroStyle.heroContent}>
-          <h1>A new kind \n of Soda</h1>
+          <h1>
+            A new kind \n of Soda <span className="tm-symbol">™</span>
+          </h1>
         </div>
         <div className={heroStyle.imageContainer}>
           <img src="src/assets/heroImage.jpg" alt="heroImage" />
