574402cfdaca9594e3be6e7f23895a39f88d1a08
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-08 12:06:07 +0200

    added html elements in Created IngrediantCard.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,12 @@
 import Hero from "../../pages/Hero/Hero";
 import mainStyle from "../Main/Main.module.css";
+import IngredientCard from "../../pages/IngredientCard/IngredientCard";
 
 export default function Main() {
   return (
     <div className={mainStyle.body}>
       <Hero></Hero>
+      <IngredientCard />
     </div>
   );
 }
---
+++ src/pages/IngredientCard/IngredientCard.tsx
@@ -0,0 +1,22 @@
+export default function IngredientCard() {
+  return (
+    <>
+      <section>
+        <div>
+          <img src="src/assets/IngredientCardImage.jpg" alt="" />
+        </div>
+
+        <div>
+          <h1>Testing Testing</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+            magni rem eveniet molestias quod ipsum velit quo, sint maxime esse
+            quibusdam blanditiis asperiores dolor earum expedita vero deserunt
+            veritatis delectus.
+          </p>
+          <button>btn</button>
+        </div>
+      </section>
+    </>
+  );
+}
