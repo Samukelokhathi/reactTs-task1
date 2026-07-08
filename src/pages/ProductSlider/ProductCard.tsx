d2208baa492cf76371875d54d2d14733f4522786
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-08 15:50:57 +0200

    Added type ItemCardProps in  productCardTsx

--- src/pages/ProductSlider/ProductCard.tsx
+++ src/pages/ProductSlider/ProductCard.tsx
@@ -1,3 +1,21 @@
-export default function ProductCard() {
-  return <div>ProductCard</div>;
-}
+import type React from "react";
+
+type ItemCardProps = {
+  name: string;
+  description: string;
+  imgLink: string;
+  btn: any;
+};
+
+// export default function ProductCard({
+//   name,
+//   imgLink,
+//   btn,
+//   description,
+// }: ItemCardProps) {
+//   return (
+//     <article>
+//         <img className={style} src={imgLink} alt={description} />
+//     </article>
+//   )
+// }
