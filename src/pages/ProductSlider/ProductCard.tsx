920fd54b4b453aa49c36a04aa734aa1baec410ba
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-08 15:59:31 +0200

    Added article element in Product card.tsx

--- src/pages/ProductSlider/ProductCard.tsx
+++ src/pages/ProductSlider/ProductCard.tsx
@@ -1,21 +1,25 @@
-import type React from "react";
+import cardStyle from "../ProductSlider/Slider.module.css";
+import { Text } from "../../components/Text/Text";
+import Button from "../../components/Button/Button";
 
 type ItemCardProps = {
   name: string;
   description: string;
   imgLink: string;
-  btn: any;
+  btnText: string;
 };
 
-// export default function ProductCard({
-//   name,
-//   imgLink,
-//   btn,
-//   description,
-// }: ItemCardProps) {
-//   return (
-//     <article>
-//         <img className={style} src={imgLink} alt={description} />
-//     </article>
-//   )
-// }
+export default function ProductCard({
+  name,
+  imgLink,
+  btnText,
+  description,
+}: ItemCardProps) {
+  return (
+    <article>
+      <img className={cardStyle["item-card"]} src={imgLink} alt={description} />
+      <Text variant={"span"} children={name} />
+      <Button text={btnText} />
+    </article>
+  );
+}
