74593785cdf87be5fefc442949eb8fa6ffe33ca8
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-08 14:55:42 +0200

    connect Product slider to main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import Hero from "../../pages/Hero/Hero";
 import mainStyle from "../Main/Main.module.css";
 import IngredientCard from "../../pages/IngredientCard/IngredientCard";
+import ProductSlider from "../../pages/ProductSlider/ProductSlider";
 
 export default function Main() {
   return (
     <div className={mainStyle.mainContent}>
       <Hero></Hero>
       <IngredientCard />
+      <ProductSlider></ProductSlider>
     </div>
   );
 }
---
+++ src/pages/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,12 @@
+import ProductCard from "./ProductCard";
+
+export default function ProductSlider() {
+  return (
+    <div>
+      <h1>Product slider tsx</h1>
+      <h1>
+        <ProductCard></ProductCard>
+      </h1>
+    </div>
+  );
+}
