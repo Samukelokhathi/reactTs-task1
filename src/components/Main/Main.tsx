0103f7786f6cd1d7d6f22e6c453f53eabe4f69a0
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-09 13:42:28 +0200

    Added Subscription page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import Hero from "../../pages/Hero/Hero";
 import mainStyle from "../Main/Main.module.css";
 import IngredientCard from "../../pages/IngredientCard/IngredientCard";
 import ProductSlider from "../../pages/ProductSlider/ProductSlider";
+import Subscription from "../../pages/Subscription-Banner/Subscription";
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <Hero></Hero>
       <IngredientCard />
       <ProductSlider></ProductSlider>
+      <Subscription />
     </div>
   );
 }
