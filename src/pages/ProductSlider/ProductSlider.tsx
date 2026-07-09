00fd2f82750376df665f9f4607bf5ddf506a4548
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-09 10:41:58 +0200

    Added heading into Product slider section

--- src/pages/ProductSlider/ProductSlider.tsx
+++ src/pages/ProductSlider/ProductSlider.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import ProductCard from "./ProductCard";
+import sliderCss from "./Slider.module.css";
+import { Text } from "../../components/Text/Text";
+
 import img1 from "../../assets/2cans.jpg";
 import img2 from "../../assets/soda-can-hero-image.jpg";
 import img3 from "../../assets/heroImage.jpg";
@@ -11,4 +14,13 @@ export default function ProductSlider() {
     { name: "img2", imgLink: img2, description: "Image no.2", btnText: "btn2" },
     { name: "img3", imgLink: img3, description: "Image no.3", btnText: "btn3" },
   ]);
+
+  return (
+    <section className={sliderCss.sliderSection}>
+      <div>
+        <Text variant={"h1"} children={"Product Slider Header"} />
+        <Text variant={"p"} children={"Hello world my name is sam"} />
+      </div>
+    </section>
+  );
 }
