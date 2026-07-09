c63f2b00b4d298ca4035d8cf41a002eb791660ea
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-09 15:59:00 +0200

    Added footer structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,44 @@
 import React from "react";
 import footerStyle from "./Footer.module.css";
+import { Text } from "../Text/Text";
+import Button from "../Button/Button";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <Text variant={"h1"} children={"Stay connected"} />
+          <input type="text" />
+          <Button text={"Subscribe"} />
+        </div>
+        <div>
+          <Text variant={"h3"} children={"FLAVOURS"} />
+          <Text variant={"P"} children={"jfdhsgjkhsdkh"} />
+        </div>
+        <div>
+          <Text variant={"h3"} children={"FLAVOURS"} />
+          <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Terms</li>
+            <li>Privacy</li>
+            <li>FAQ</li>
+          </ul>
+          <Text variant={"P"} children={"jfdhsgjkhsdkh"} />
+        </div>
+        <div>
+          <Text variant={"h3"} children={"FLAVOURS"} />
+          <ul>
+            <li>i</li>
+            <li>f</li>
+            <li>t</li>
+            <li>l</li>
+            <li>p</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
