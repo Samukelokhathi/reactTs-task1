2fec583b1df662e1fb0815c9db18bba1491957c4
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-09 10:32:12 +0200

    Added object in product slider.tsx component

--- src/pages/ProductSlider/ProductSlider.tsx
+++ src/pages/ProductSlider/ProductSlider.tsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import ProductCard from "./ProductCard";
+import img1 from "../../assets/2cans.jpg";
+import img2 from "../../assets/soda-can-hero-image.jpg";
+import img3 from "../../assets/heroImage.jpg";
 
 export default function ProductSlider() {
-  return (
-    <div>
-      <h1>Product slider tsx</h1>
-      <h1>
-        <ProductCard></ProductCard>
-      </h1>
-    </div>
-  );
+  const [count, setCount] = useState(0);
+  const [stock, setStock] = useState([
+    { name: "sam", imgLink: img1, description: "Image no.1", btnText: "btn1" },
+    { name: "img2", imgLink: img2, description: "Image no.2", btnText: "btn2" },
+    { name: "img3", imgLink: img3, description: "Image no.3", btnText: "btn3" },
+  ]);
 }
