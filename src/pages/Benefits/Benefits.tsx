c27fd516ae3be9bb627b4a75f459067d4c6f990c
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-10 10:27:22 +0200

    Added benefit section with icon, text and image

--- src/pages/Benefits/Benefits.tsx
+++ src/pages/Benefits/Benefits.tsx
@@ -1,5 +1,32 @@
 import benefitStyle from "./BenefitItems.module.css";
+import { Text } from "../../components/Text/Text";
 
 export default function Benefits() {
-  return <div>Benefits</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <i>icon</i>
+          <Text variant={"p"} children={"Save 15% every order"} />
+        </div>
+        <div>
+          <i>icon</i>
+          <Text variant={"p"} children={"Save 15% every order"} />
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <i>icon</i>
+          <Text variant={"p"} children={"Save 15% every order"} />
+        </div>
+        <div>
+          <i>icon</i>
+          <Text variant={"p"} children={"Save 15% every order"} />
+        </div>
+      </div>
+    </section>
+  );
 }
