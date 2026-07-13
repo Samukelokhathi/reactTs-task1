59d97508f54cf32d9f5773f926d35bcee9a9d423
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-13 14:42:01 +0200

    Added bg styling for each card items and replaced images

--- src/pages/ProductSlider/ProductSlider.tsx
+++ src/pages/ProductSlider/ProductSlider.tsx
@@ -6,7 +6,7 @@ import { Text } from "../../components/Text/Text";
 import img1 from "../../pages/ProductSlider/Asset/image-1.jpg";
 import img2 from "../../pages/ProductSlider/Asset/image-2.jpg";
 import img3 from "../../pages/ProductSlider/Asset/image-3.jpg";
-import img4 from "../../pages/ProductSlider/Asset/image-4.jpg";
+import img4 from "../../pages/ProductSlider/Asset/image-4.png";
 
 export default function ProductSlider() {
   // const [count, setCount] = useState(0);
@@ -16,28 +16,28 @@ export default function ProductSlider() {
       imgLink: img1,
       description: "Image no.1",
       btnText: "btn1",
-      styling: " rgb(233,224,203)",
+      cardBgColor: " rgb(233,224,203)",
     },
     {
       name: "img2",
       imgLink: img2,
       description: "Image no.2",
       btnText: "btn2",
-      styling: "rgb(207,191,220)",
+      cardBgColor: "rgb(207,191,220)",
     },
     {
       name: "img3",
       imgLink: img3,
       description: "Image no.3",
       btnText: "btn3",
-      styling: "rgb(224,199,175)",
+      cardBgColor: "rgb(224,199,175)",
     },
     {
       name: "img4",
       imgLink: img4,
       description: "Image no.4",
       btnText: "btn4",
-      styling: "rgb(170,212,214)",
+      cardBgColor: "rgb(170,212,214)",
     },
   ]);
 
@@ -61,7 +61,7 @@ export default function ProductSlider() {
                 btnText={product.btnText}
                 description={product.description}
                 name={product.name}
-                color={product.styling}
+                color={product.cardBgColor}
               />
             );
           })}
