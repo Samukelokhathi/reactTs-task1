4bce5a59eb6d92bc685f4a385332acbb2fb19c82
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-13 08:56:24 +0200

    run build for cli

--- src/pages/ProductSlider/ProductSlider.tsx
+++ src/pages/ProductSlider/ProductSlider.tsx
@@ -8,8 +8,8 @@ import img2 from "../../assets/soda-can-hero-image.jpg";
 import img3 from "../../assets/heroImage.jpg";
 
 export default function ProductSlider() {
-  const [count, setCount] = useState(0);
-  const [stock, setStock] = useState([
+  // const [count, setCount] = useState(0);
+  const [stock] = useState([
     { name: "sam", imgLink: img1, description: "Image no.1", btnText: "btn1" },
     { name: "img2", imgLink: img2, description: "Image no.2", btnText: "btn2" },
     { name: "img3", imgLink: img3, description: "Image no.3", btnText: "btn3" },
