58a9699de08caa97cf5f7b475916fe2de42734be
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-14 14:19:37 +0200

    added logo and flex direction in 1st row of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,27 @@
 import footerStyle from "./Footer.module.css";
 import { Text } from "../../components/Text/Text";
 import Button from "../../components/Button/Button.tsx";
+import footerLogo from "./FooterAsset/Logo Image.jpg";
 
 export default function Footer() {
   return (
-    <section>
-      <div className={footerStyle.footerContent}>
-        <div>
-          <img src="" alt="" />
-          <Text variant={"h1"} children={"Stay connected"} />
+    <section className={footerStyle["footer-section"]}>
+      <div className={footerStyle["footer-content"]}>
+        <div className={footerStyle["sub-sec-1"]}>
+          <div className={footerStyle["logo-text-div"]}>
+            <img src={footerLogo} alt="" width={50} height={50} />
+            <Text variant={"h1"} children={"Stay connected"} />
+          </div>
           <input type="text" />
           <Button text={"Subscribe"} />
         </div>
-        <div>
+
+        <div className={footerStyle["sub-sec-2"]}>
           <Text variant={"h3"} children={"FLAVOURS"} />
           <Text variant={"P"} children={"jfdhsgjkhsdkh"} />
         </div>
-        <div>
+
+        <div className={footerStyle["sub-sec-3"]}>
           <Text variant={"h3"} children={"INFORMATION"} />
           <ul>
             <li>About Us</li>
@@ -27,7 +32,8 @@ export default function Footer() {
           </ul>
           <Text variant={"P"} children={"jfdhsgjkhsdkh"} />
         </div>
-        <div>
+
+        <div className={footerStyle["sub-sec-4"]}>
           <Text variant={"h3"} children={"SOCIAL MEDIA"} />
           <ul>
             <li>i</li>
